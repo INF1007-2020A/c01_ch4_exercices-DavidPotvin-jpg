2afd0979e60056412b10c374b9f2873a2d27615b
DavidPotvin-jpg <david.potvin@polymtl.ca>
2020-09-16 18:51:45 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return str
+    return str[]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
